630caadb618d374b00e28a613c8e39f455a8c560
Justin Davis <davisjustinw@gmail.com>
2019-01-06 03:10:12 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -24,5 +24,10 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  valid_move?(board, index) ?
+    move(board, index) : turn(board)
+  display_board(board)
   
 end
